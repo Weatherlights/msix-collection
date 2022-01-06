605766611ebab8bd0f10488643dea01496333ab9
Weatherlights <hauke@hauke.us>
2022-01-06 21:07:57 +0100

    Added Scripts,. 7-Zip and Notepad++

---
+++ Scripts/Packing.ps1
@@ -0,0 +1,27 @@
+﻿param(
+    $OutFile = "C:\Users\hauke\OneDrive - weatherlights.com\Documents\Developing\MSIXSuite\Software\AutoHotkey\x64.appx",
+    $Folder = "C:\Users\hauke\OneDrive - weatherlights.com\Documents\Developing\MSIXSuite\Software\AutoHotkey\x64",
+    $SignFile = $true
+);
+
+$sdkfolder = "C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64";
+$MakeAppxEXE = "$sdkfolder\MakeAppx.exe"
+$SignTool = "$sdkfolder\SignTool.exe"
+$MakePri = "$sdkfolder\MakePri.exe"
+
+$arguments = "pack /p `"$OutFile`" /d `"$Folder`""
+Start-Process $MakeAppxEXE -ArgumentList $arguments -Wait -NoNewWindow -PassThru
+# & $MakeAppxEXE $arguments;
+#.\MakeAppx.exe pack /p `"$OutFile`" /d `"$Folder`"
+
+#cd $Folder;
+
+#& $MakePri createconfig /cf ..\pri\configuration.xml /dq en-us_de-de /pv 10.0 /o
+#& $MakePri new /pr . /cf ..\pri\configuration.xml /of ..\pri\resources.pri /mf AppX /o
+#& $MakeAppxEXE pack /m AppxManifest.xml /f ../pri/resources.map.txt /p "$OutFile" /o
+#& $SignTool sign /a /v /fd SHA256 "$OutFile"
+
+if ( $SignFile ) {
+    $arguments = "sign /a /v /tr http://timestamp.digicert.com /fd SHA256 `"$OutFile`""
+    Start-Process $SignTool -ArgumentList $arguments -Wait -NoNewWindow -PassThru
+}
---
+++ Scripts/Unpacking.ps1
@@ -0,0 +1,16 @@
+﻿param(
+    $File = "C:\Users\hauke\downloads\npp.msix",
+    $OutFolder = "C:\Users\hauke\Downloads\mynppx86"
+);
+
+$sdkfolder = "C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64";
+$MakeAppxEXE = "$sdkfolder\MakeAppx.exe"
+$extension = ($File -split "\.")[($File -split "\.").Count-1]
+
+if ( $extension -eq "appxbundle") {
+    $arguments = "unbundle /p `"$File`" /d `"$OutFolder`""
+} else {
+    $arguments = "unpack /p `"$File`" /d `"$OutFolder`""
+}
+
+Start-Process $MakeAppxEXE -ArgumentList $arguments -Wait -NoNewWindow -PassThru
